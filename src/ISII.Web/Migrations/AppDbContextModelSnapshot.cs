d954f7e36fa8db05cba958c186fc89e1f6834828
alexiswalker <alexiswalker@hotmail.com>
2026-08-29 10:55:27 -0300

    rename and update initial migration to new namespace and naming convention

--- src/ISII.Web/Migrations/AppDbContextModelSnapshot.cs
+++ src/ISII.Web/Migrations/AppDbContextModelSnapshot.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace ISII.Web.Infrastructure.Data.Migrations
+namespace ISII.Web.Migrations
 {
     [DbContext(typeof(AppDbContext))]
     partial class AppDbContextModelSnapshot : ModelSnapshot
